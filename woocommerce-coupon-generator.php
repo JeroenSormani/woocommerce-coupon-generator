e6166a3e149b40d43b408164ad160efde234349a
JeroenSormani <jeroen.sormani@gmail.com>
2015-08-29 09:59:41 +0200

    Add WooCommerce activated check

--- woocommerce-coupon-generator.php
+++ woocommerce-coupon-generator.php
@@ -95,6 +95,16 @@ class WooCommerce_Coupon_Generator {
 	 */
 	public function init() {
 
+		// Check if WooCommerce is active
+		if ( ! function_exists( 'is_plugin_active_for_network' ) ) :
+			require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+		endif;
+
+		if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) :
+			if ( ! is_plugin_active_for_network( 'woocommerce/woocommerce.php' ) ) :
+				return;
+			endif;
+		endif;
 
 		if ( is_admin() ) :
 
